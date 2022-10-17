975710b8b135d7b3af68ae27e77922a39c831299
terwer <youweics@163.com>
2022-10-17 17:09:14 +0800

    Fix sqlplus connect

--- util.sql
+++ util.sql
@@ -11,12 +11,13 @@ select username as schema_name from sys.all_users order by username;
 
 -- sqlplus
 -- sqlplus sys/123456 as sysdba
+-- sqlplus sys/123456@//localhost:1521/XE as sysdba
 
 -- 创建临时表空间
-CREATE TEMPORARY tablespace my_temp tempfile '/opt/oracle/oradata/my_temp.dbf' SIZE 50m autoextend ON NEXT 50m maxsize 20480m extent management LOCAL;
+CREATE TEMPORARY tablespace my_temp tempfile '/u01/app/oracle/oradata/XE/my_temp.dbf' SIZE 50m autoextend ON NEXT 50m maxsize 20480m extent management LOCAL;
 
 -- 创建数据表空间
-CREATE tablespace my_data logging datafile '/opt/oracle/oradata/my_data.dbf' SIZE 50m autoextend ON NEXT 50m maxsize 20480m extent management LOCAL;
+CREATE tablespace my_data logging datafile '/u01/app/oracle/oradata/XE/my_data.dbf' SIZE 50m autoextend ON NEXT 50m maxsize 20480m extent management LOCAL;
 
 -- create user kms15 identified by 123456 default tablespace my_data temporary tablespace my_temp;
 -- grant CONNECT,resource,dba to kms15;
